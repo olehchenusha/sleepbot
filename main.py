920877af3776144dbb5ba359903177199fe022e4
Oleg Chenusha <oleg.chenusha@zxteam.net>
2023-02-07 15:36:13 +0200

    Fixed MessageHandlers

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+#
+
 from dotenv import load_dotenv
 import os
 
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -13,32 +16,44 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Hi! I'm sleep bot, i can control your time of sleeping!"
+        text="Hi! I'm sleep bot, I can control your time of sleeping!"
     )
 
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(
         chat_id=update.effective_chat.id, 
-        text="Help info"
+        text="Help info" # TBD
     )
 
 async def fallAsleep(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Good night! I saved the time when you went to bed"
+    )
+    # TBD
 
 async def wakeUp(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Good Morning!\n\nYou fell asleep in:\nYou woke up in:"
+    )
+    # TBD
 
-async def statistick(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+async def statistics(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, 
+        text="Your stats by the day:\nMonday:\nTuesday\nWednesday:\nThursday:\nFriday:\nSaturday:\nSunday:\n"
+    )
+    # TBD 
 
 if __name__ == '__main__':
     load_dotenv()
     app = ApplicationBuilder().token(os.getenv('TOKEN')).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler("How it works?", info))
-    app.add_handler(MessageHandler("Sleep", fallAsleep))
-    app.add_handler(MessageHandler("Wake up", wakeUp))
-    app.add_handler(MessageHandler("Show statistick", statistick))
+    app.add_handler(MessageHandler(filters.Text('Lets go!'), info))
+    app.add_handler(MessageHandler(filters.Text('Go sleep'), fallAsleep))
+    app.add_handler(MessageHandler(filters.Text("I'm woke up"), wakeUp))
+    app.add_handler(MessageHandler(filters.Text('Stats'), statistics))
 
     app.run_polling()
